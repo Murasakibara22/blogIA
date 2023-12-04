2143de162b9baa37615248d6439168060f4a4ef5
Murasakibara22 <joackimcoulibaly5@gmail.com>
2023-12-04 14:29:57 +0000

    TS-user: mise en place de la fonctionnalite liste des utilisateurs

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -139,7 +139,7 @@
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link {{ Request::is('utilisateurs/liste') ? 'active':'' }}" href="../dashboard/app/user-list.html">
+                                <a class="nav-link {{ Request::is('dashboard/utilisateurs/liste') ? 'active':'' }}" href="/dashboard/utilisateurs/liste">
                                     <i class="icon">
                                         <svg class="icon-10" width="10" viewBox="0 0 24 24" fill="currentColor"
                                             xmlns="http://www.w3.org/2000/svg">
